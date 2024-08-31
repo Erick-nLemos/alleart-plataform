fcb6f4d70f8b1bdad3fbc48bb55c015023816535
Erick Lemos <ericklemos.dev@gmail.com>
2024-08-31 20:48:47 -0300

    First commit App AlleArt

---
+++ app/src/main/java/com/ericklemos/alleart/Cursos.java
@@ -0,0 +1,18 @@
+package com.ericklemos.alleart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Cursos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cursos);
+    }
+
+    public void clickVoltar(View view){
+        finish();
+    }
+}
---
+++ app/src/main/java/com/ericklemos/alleart/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ericklemos.alleart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    // o que torna possível a ligação entre
+    //as atividades é o atributo
+    //chamado ‘Intent’
+    Intent tela;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickEquipe(View view){
+        tela = new Intent(getApplicationContext(), Equipe.class);
+        startActivity(tela);
+    }
+
+    public void clickCursos(View view){
+        tela = new Intent(getApplicationContext(), Cursos.class);
+        startActivity(tela);
+    }
+}

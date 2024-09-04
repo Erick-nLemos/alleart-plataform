bfb335ea65d1aab25ce694158b3ded468c5bf7f0
Erick Lemos <ericklemos.dev@gmail.com>
2024-09-04 18:09:41 -0300

    Tela Cursos Concluida

--- app/src/main/java/com/ericklemos/alleart/Cursos.java
+++ app/src/main/java/com/ericklemos/alleart/Cursos.java
@@ -3,9 +3,12 @@ package com.ericklemos.alleart;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
 
 public class Cursos extends AppCompatActivity {
 
+    Intent nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
